6bb3015eb6ac61df68de27cbd0aa3f52815e22d4
Anil Kishore <anil.daiict@gmail.com>
2022-12-02 23:15:53 -0800

    d3 pt2

--- src/Day03.kt
+++ src/Day03.kt
@@ -10,7 +10,10 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence().chunked(3).map {
+            val ch = it[0].asIterable().intersect(it[1].asIterable()).intersect(it[2].asIterable()).first()
+            if (ch in 'a'..'z') (ch - 'a' + 1) else 26 + (ch - 'A' + 1)
+        }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
