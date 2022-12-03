564f96f0de3eb33688983468070b50fb36ef2083
Anil Kishore <anil.daiict@gmail.com>
2022-12-02 23:09:11 -0800

    d3 pt1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,24 @@
 fun main() {
-    
+
+    fun part1(input: List<String>): Int {
+        return input.asSequence().map {
+            val first = it.substring(0, it.length / 2).asIterable()
+            val second = it.substring(it.length / 2).asIterable()
+            val ch = first.intersect(second).first()
+            if (ch in 'a'..'z') (ch - 'a' + 1) else 26 + (ch - 'A' + 1)
+        }.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    println(part1(testInput))
+    check(part1(testInput) == 157)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
 }
