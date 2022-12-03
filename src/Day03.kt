3bb0c71f00bece2b8c87ceef3f33ed6e82273f9c
Anil Kishore <anil.daiict@gmail.com>
2022-12-03 14:12:56 -0800

    map reduce

--- src/Day03.kt
+++ src/Day03.kt
@@ -11,15 +11,10 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         return input.asSequence().chunked(3).map {
-            val ch = it[0].asIterable().intersect(it[1].toSet()).intersect(it[2].toSet()).first()
+            val ch = it.map { s -> s.toSet() }.reduce { a, b -> a.intersect(b)}.first()
             if (ch in 'a'..'z') (ch - 'a' + 1) else 26 + (ch - 'A' + 1)
         }.sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day03_test")
-    println(part1(testInput))
-    check(part1(testInput) == 157)
 
     val input = readInput("Day03")
     println(part1(input))
