93b0a167b51773eb6ced0ba9d2d74ee0986bc2eb
Anil Kishore <anil.daiict@gmail.com>
2022-12-05 22:14:40 -0800

    Update Day06.kt

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,17 +2,13 @@ fun main() {
 
     fun solve(str: String, lim: Int): Int {
         val arr = str.toCharArray()
-        for(i in lim..  str.length) {
-            if (hashSetOf(*arr.copyOfRange(i-lim, i).toTypedArray()).size == lim)
+        for (i in lim..str.length)
+            if (hashSetOf(*arr.copyOfRange(i - lim, i).toTypedArray()).size == lim)
                 return i
-        }
         return -1
     }
 
-    fun part1(str: String) = solve(str, 4)
-    fun part2(str: String) = solve(str, 14)
-
     val input = readInput("Day06")
-    println(part1(input[0]))
-    println(part2(input[0]))
+    println(solve(input[0], 4))
+    println(solve(input[0], 14))
 }
