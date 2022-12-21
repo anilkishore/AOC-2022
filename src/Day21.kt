cbf08d37544de97234cf37e3ff0b65dedfd59857
Anil Kishore <anil.daiict@gmail.com>
2022-12-21 15:25:24 -0800

    D21 update

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,9 +1,43 @@
 import java.lang.Exception
 
-fun add(a: Long, b: Long): Long = a + b
-fun subtract(a: Long, b: Long): Long = a - b
-fun multiply(a: Long, b: Long): Long = a * b
-fun divide(a: Long, b: Long): Long = a / b
+sealed class Operation {
+    abstract fun run(a: Long, b: Long): Long
+    abstract fun getA(b: Long, result: Long): Long
+    abstract fun getB(a: Long, result: Long): Long
+}
+
+object Add : Operation() {
+    override fun run(a: Long, b: Long) = a + b
+
+    override fun getA(b: Long, result: Long) = result - b
+
+    override fun getB(a: Long, result: Long) = result - a
+}
+
+object Subtract : Operation() {
+    override fun run(a: Long, b: Long) = a - b
+
+    override fun getA(b: Long, result: Long) = result + b
+
+    override fun getB(a: Long, result: Long) = a - result
+}
+
+object Multiply : Operation() {
+    override fun run(a: Long, b: Long) = a * b
+
+    override fun getA(b: Long, result: Long) = result / b
+
+    override fun getB(a: Long, result: Long) = result / a
+}
+
+object Divide : Operation() {
+    override fun run(a: Long, b: Long) = a / b
+
+    override fun getA(b: Long, result: Long) = result * b
+
+    override fun getB(a: Long, result: Long) = a / result
+}
+
 
 sealed class Variable {
     abstract fun getValue(): Long
@@ -13,45 +47,25 @@ class NumberVariable(private val value: Long) : Variable() {
     override fun getValue(): Long = value
 }
 
-class OperationVariable(val a: String, val b: String, val op: (Long, Long) -> Long) : Variable() {
-    override fun getValue(): Long = op(findValue(a), findValue(b))
-    fun evaluate(a: Long, b: Long): Long = op(a, b)
-    fun findA(bVal: Long, expValue: Long): Long {
-        return when (op) {
-            ::add -> expValue - bVal
-            ::subtract -> expValue + bVal
-            ::multiply -> expValue / bVal
-            ::divide -> expValue * bVal
-            else -> error("No-op")
-        }
-    }
-
-    fun findB(aVal: Long, expValue: Long): Long {
-        return when (op) {
-            ::add -> expValue - aVal
-            ::subtract -> aVal - expValue
-            ::multiply -> expValue / aVal
-            ::divide -> aVal / expValue
-            else -> error("No-op")
-        }
-    }
+class OperationVariable(val a: String, val b: String, val op: Operation) : Variable() {
+    override fun getValue(): Long = op.run(findValue(a), findValue(b))
 }
+
+class Result(val value: Long, val isAlongHumn: Boolean)
 
 val params = mutableMapOf<String, Variable>()
-val valuePairs = mutableMapOf<String, Pair<Long, Boolean>>()
-fun findValue(str: String): Long {
-    val variable = params[str] ?: throw Exception("Unknown param $str")
-    return variable.getValue()
-}
+val valuePairs = mutableMapOf<String, Result>()
 
-fun fillValuePairs(str: String): Pair<Long, Boolean> {
+fun findValue(str: String): Long = (params[str] ?: throw Exception("Unknown param $str")).getValue()
+
+fun fillValuePairs(str: String): Result {
     val variable = params[str] ?: throw Exception("Unknown param $str")
     val res = when (variable) {
-        is NumberVariable -> Pair(variable.getValue(), str == "humn")
+        is NumberVariable -> Result(variable.getValue(), str == "humn")
         is OperationVariable -> {
             val ap = fillValuePairs(variable.a)
             val bp = fillValuePairs(variable.b)
-            Pair(variable.evaluate(ap.first, bp.first), ap.second or bp.second)
+            Result(variable.op.run(ap.value, bp.value), ap.isAlongHumn or bp.isAlongHumn)
         }
     }
     valuePairs[str] = res
@@ -64,27 +78,25 @@ fun trickle(str: String, expValue: Long) {
         return
     }
     val variable = params[str] as? OperationVariable ?: error("Not OperationVariable")
-    if (valuePairs[variable.a]!!.second) {
-        // ? op b
-        val bVal = valuePairs[variable.b]!!.first
-        val aVal = variable.findA(bVal, expValue)
+    val resA = valuePairs[variable.a] ?: error("")
+    val resB = valuePairs[variable.b] ?: error("")
+    if (resA.isAlongHumn) { // ? op b = exp
+        val aVal = variable.op.getA(resB.value, expValue)
         trickle(variable.a, aVal)
-    } else {
-        // a op ?
-        val aVal = valuePairs[variable.a]!!.first
-        val bVal = variable.findB(aVal, expValue)
+    } else { // a op ? = exp
+        val bVal = variable.op.getB(resA.value, expValue)
         trickle(variable.b, bVal)
     }
 }
 
 fun main() {
 
-    fun getOp(op: String): (Long, Long) -> Long {
+    fun getOperation(op: String): Operation {
         return when (op) {
-            "+" -> ::add
-            "-" -> ::subtract
-            "*" -> ::multiply
-            "/" -> ::divide
+            "+" -> Add
+            "-" -> Subtract
+            "*" -> Multiply
+            "/" -> Divide
             else -> throw Exception("Unknown operator $op")
         }
     }
@@ -94,7 +106,7 @@ fun main() {
             val ss = line.split(": ", " ")
             params[ss[0]] =
                 if (ss.size == 2) NumberVariable(ss[1].toLong())
-                else OperationVariable(ss[1], ss[3], getOp(ss[2]))
+                else OperationVariable(ss[1], ss[3], getOperation(ss[2]))
         }
     }
 
@@ -105,10 +117,10 @@ fun main() {
     fun part2() {
         val root = params["root"] as? OperationVariable ?: error("No root")
         fillValuePairs("root")
-        if (valuePairs[root.a]!!.second)
-            trickle(root.a, valuePairs[root.b]!!.first)
+        if (valuePairs[root.a]!!.isAlongHumn)
+            trickle(root.a, valuePairs[root.b]!!.value)
         else
-            trickle(root.b, valuePairs[root.a]!!.first)
+            trickle(root.b, valuePairs[root.a]!!.value)
     }
 
     val input = readInput("Day21")
