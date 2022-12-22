2a64e3bb1d2bdccab0580bff37be7dabc93946f0
Anil Kishore <anil.daiict@gmail.com>
2022-12-21 16:02:18 -0800

    21

--- src/Day21.kt
+++ src/Day21.kt
@@ -5,48 +5,33 @@ sealed class Operation {
     abstract fun getA(b: Long, result: Long): Long
     abstract fun getB(a: Long, result: Long): Long
 }
-
 object Add : Operation() {
     override fun run(a: Long, b: Long) = a + b
-
     override fun getA(b: Long, result: Long) = result - b
-
     override fun getB(a: Long, result: Long) = result - a
 }
-
 object Subtract : Operation() {
     override fun run(a: Long, b: Long) = a - b
-
     override fun getA(b: Long, result: Long) = result + b
-
     override fun getB(a: Long, result: Long) = a - result
 }
-
 object Multiply : Operation() {
     override fun run(a: Long, b: Long) = a * b
-
     override fun getA(b: Long, result: Long) = result / b
-
     override fun getB(a: Long, result: Long) = result / a
 }
-
 object Divide : Operation() {
     override fun run(a: Long, b: Long) = a / b
-
     override fun getA(b: Long, result: Long) = result * b
-
     override fun getB(a: Long, result: Long) = a / result
 }
-
 
 sealed class Variable {
     abstract fun getValue(): Long
 }
-
 class NumberVariable(private val value: Long) : Variable() {
     override fun getValue(): Long = value
 }
-
 class OperationVariable(val a: String, val b: String, val op: Operation) : Variable() {
     override fun getValue(): Long = op.run(findValue(a), findValue(b))
 }
@@ -54,32 +39,31 @@ class OperationVariable(val a: String, val b: String, val op: Operation) : Varia
 class Result(val value: Long, val isAlongHumn: Boolean)
 
 val params = mutableMapOf<String, Variable>()
-val valuePairs = mutableMapOf<String, Result>()
-
-fun findValue(str: String): Long = (params[str] ?: throw Exception("Unknown param $str")).getValue()
+val resultsMap = mutableMapOf<String, Result>()
+private const val HUMN = "humn"
+fun findValue(str: String): Long = (params[str] ?: error("!!")).getValue()
 
 fun fillValuePairs(str: String): Result {
-    val variable = params[str] ?: throw Exception("Unknown param $str")
-    val res = when (variable) {
-        is NumberVariable -> Result(variable.getValue(), str == "humn")
+    val res = when (val variable = params[str] ?: error("Unknown param $str")) {
+        is NumberVariable -> Result(variable.getValue(), str == HUMN)
         is OperationVariable -> {
-            val ap = fillValuePairs(variable.a)
-            val bp = fillValuePairs(variable.b)
-            Result(variable.op.run(ap.value, bp.value), ap.isAlongHumn or bp.isAlongHumn)
+            val aRes = fillValuePairs(variable.a)
+            val bRes = fillValuePairs(variable.b)
+            Result(variable.op.run(aRes.value, bRes.value), aRes.isAlongHumn or bRes.isAlongHumn)
         }
     }
-    valuePairs[str] = res
+    resultsMap[str] = res
     return res
 }
 
 fun trickle(str: String, expValue: Long) {
-    if (str == "humn") {
+    if (str == HUMN) {
         println("Part 2 ans: $expValue")
         return
     }
     val variable = params[str] as? OperationVariable ?: error("Not OperationVariable")
-    val resA = valuePairs[variable.a] ?: error("")
-    val resB = valuePairs[variable.b] ?: error("")
+    val resA = resultsMap[variable.a] ?: error("")
+    val resB = resultsMap[variable.b] ?: error("")
     if (resA.isAlongHumn) { // ? op b = exp
         val aVal = variable.op.getA(resB.value, expValue)
         trickle(variable.a, aVal)
@@ -117,10 +101,10 @@ fun main() {
     fun part2() {
         val root = params["root"] as? OperationVariable ?: error("No root")
         fillValuePairs("root")
-        if (valuePairs[root.a]!!.isAlongHumn)
-            trickle(root.a, valuePairs[root.b]!!.value)
+        if (resultsMap[root.a]!!.isAlongHumn)
+            trickle(root.a, resultsMap[root.b]!!.value)
         else
-            trickle(root.b, valuePairs[root.a]!!.value)
+            trickle(root.b, resultsMap[root.a]!!.value)
     }
 
     val input = readInput("Day21")
