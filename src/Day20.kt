f8f989fa400c2fac42a5eb3b0aa99d0c01bc08a9
Anil Kishore <anil.daiict@gmail.com>
2022-12-21 23:55:47 -0800

    D20

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,27 +1,51 @@
 fun main() {
 
-    fun part1(nums: List<Int>): Int {
+    fun part1(nums: List<Long>, mult: Long = 1, times: Int = 1): Long {
         val n = nums.size
-        val numList = mutableListOf<Pair<Int, Int>>()
+        val numList = mutableListOf<Pair<Long, Int>>()
         for (i in nums.indices) {
-            numList.add(Pair(nums[i], i))
+            numList.add(Pair(nums[i] * mult, i))
         }
-        for (i in nums.indices) {
-            var p = 0
 
-            for(j in nums.indices) {
-                if (numList[j].second == i) {
-                    p = j
-                    break
+        repeat(times) {
+            for (i in nums.indices) {
+                var p = 0
+
+                for (j in nums.indices) {
+                    if (numList[j].second == i) {
+                        p = j
+                        break
+                    }
                 }
-            }
 
-            // 1, 2, -3, 3, -2, 0, 4
-            val k = numList[p].first
+                val k = numList[p].first
+                if (k == 0L) continue
+                val elem = numList.removeAt(p)
+                var nk = ((p + k % (n - 1) + (n - 1)) % (n - 1)).toInt()
+                if (nk == 0) nk = (n - 1)
+                numList.add(nk, elem)
+            }
         }
-        return 0
+
+        var p = 0
+        for (j in nums.indices) {
+            if (numList[j].first == 0L) {
+                p = j
+                break
+            }
+        }
+
+        return listOf(1000, 2000, 3000).sumOf { k ->
+            val nk = (p + k) % n
+            numList[nk].first
+        }
     }
 
-    val input = readInput("Day20").map { it.toInt() }
+    fun part2(nums: List<Long>): Long {
+        return part1(nums, 811589153L, 10)
+    }
+
+    val input = readInput("Day20").map { it.toLong() }
     println(part1(input))
+    println(part2(input))
 }
